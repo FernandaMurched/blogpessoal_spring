060c1ef7ed45c555cdc1d9cfffe63cdc6e1c35b3
Fernanda Murched <fernanda_hco@outlook.com.br>
2025-06-23 10:44:34 -0300

    :rocket: Deploy da aplicação

--- src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -50,6 +50,7 @@ public class UsuarioService {
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
 
 			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+			usuario.setId(null);
 
 			return Optional.ofNullable(usuarioRepository.save(usuario));
 
